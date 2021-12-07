8ba5f99ace3fb018122a19158c91eba80b7aa36d
carina <benazirihtiyorova22@gmail.com>
2021-12-07 15:05:17 -0800

    added imgslider component

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+function Home() {
+  return (
+    <Container>
+      <ImgSlider />
+      {/* <Viewers />
+      <Movies /> */}
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vh + 5px);
+  position: relative;
+  overflow-x: hidden;
+  &:before {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    z-index: -1;
+  }
+`;
+// const ImgSlider = styled.img
